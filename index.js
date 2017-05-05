a8c72ad80d7014390b720453ec60112a88bfb546
Alhasanj90 <a.j.alhefdhi@hotmail.com>
2017-05-05 20:33:12 +0100

    okay

--- index.js
+++ index.js
@@ -1,7 +1,161 @@
 var express = require('express');
+var mongoose = require ("mongoose");
+
+var uristring = 
+  process.env.MONGODB_URI || 
+  'mongodb://localhost/HelloMongoose';
+
+
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+//
+// This is the schema.  Note the types, validation and trim
+// statements.  They enforce useful constraints on the data.
+var PlayerSchema = new mongoose.Schema({
+    name: String,
+    nationality: String,
+    rank: Number,
+    team      : {
+        previous : String,
+        current  : String
+    },
+  age: { type: Number, min: 18}
+});
+
+// Compiles the schema into a model, opening (or creating, if
+// nonexistent) the 'PowerUsers' collection in the MongoDB database
+var player = mongoose.model('Player', PlayerSchema);
+
+
+// Clear out old data
+player.remove({}, function(err) {
+  if (err) {
+    console.log ('error deleting old data.');
+  }
+});
+
+// Creating one user.
+var messi = new player ({
+  name: 'L.Messi',
+  age: 25,
+  nationality: 'Argentina',
+  team: {current : "Barcelona",
+		previous: ''
+		},
+		rank : 9.6
+});
+ 
+messi.save(function (err) {if (err) console.log ('Error on save!')});
+
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+
+
+router.route('/players')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    // .post(function(req, res) {
+        
+    //     var player = new Player();      // create a new instance of the Bear model
+    //     player.name = req.body.name;  // set the bears name (comes from the request)
+
+    //     // save the bear and check for errors
+    //     player.save(function(err) {
+    //         if (err)
+    //             res.send(err);
+
+    //         res.json({ message: 'Bear created!' });
+    //     });
+        
+    // });
+
+	// get all the bears (accessed at GET http://localhost:8080/api/bears)
+	.get(function(req, res) {
+		Player.find(function(err, players) {
+			if (err)
+				res.send(err);
+
+			res.json(players);
+		});
+	});
+
+// // on routes that end in /bears/:bear_id
+// // ----------------------------------------------------
+// router.route('/players/:player_id')
+
+// 	// get the bear with that id
+// 	.get(function(req, res) {
+// 		Player.findById(req.params.player_id, function(err, bear) {
+// 			if (err)
+// 				res.send(err);
+// 			res.json(player);
+// 		});
+// 	})
+
+// 	// update the bear with this id
+// 	.put(function(req, res) {
+// 		Player.findById(req.params.bear_id, function(err, bear) {
+
+// 			if (err)
+// 				res.send(err);
+
+// 			player.name = req.body.name;
+// 			bear.save(function(err) {
+// 				if (err)
+// 					res.send(err);
+
+// 				res.json({ message: 'Bear updated!' });
+// 			});
+
+// 		});
+// 	})
+
+// 	// delete the bear with this id
+// 	.delete(function(req, res) {
+// 		Bear.remove({
+// 			_id: req.params.bear_id
+// 		}, function(err, bear) {
+// 			if (err)
+// 				res.send(err);
+
+// 			res.json({ message: 'Successfully deleted' });
+// 		});
+// 	});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });	
+});
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+
+
+
 
 app.use(express.static(__dirname + '/public'));
 
